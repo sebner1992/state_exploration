464912ab7b35cc56d37828e3089d07568b8cc03b
Sebastian Ebner <sebastian.ebner@porscheinformatik.at>
2017-12-19 10:06:55 +0100

    adapted Test
    Change-Id: I6c8b439eb6a77576a439100cb47ffcec44b204e6

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,12 +1,20 @@
 package test;
 
 import assignment8_int.P1;
+import assignment8_int.P2;
 
-public class Test {
+public class Test
+{
 
-	public static void main(String[] args) {
-		P1 p1 = new averkova_ebner.p1.Problem1();  //note the default constructor
-		int x = p1.solve(4, 9, 5, System.out);
-	}
+    public static void main(String[] args)
+    {
+        P1 p1 = new averkova_ebner.p1.Problem1(); //note the default constructor
+        int x = p1.solve(4, 9, 5, System.out);
+
+        System.out.println("-----------------------------------------");
+
+        P2 p2 = new averkova_ebner.p2.Problem2();
+        int y = p2.solve(2, 4, 6, 1, 2, 3, 6, System.out);
+    }
 
 }
